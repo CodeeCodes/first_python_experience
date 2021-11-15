2e8894e5023e066519eecaefd2a21f60a6147823
Codee Palmer <codeecodes92@gmail.com>
2021-11-15 10:37:44 -0500

    Fixing commit

---
+++ ap.py
@@ -0,0 +1,39 @@
+# variables & strings
+character_name = "Codee"
+print(character_name + " Is High")
+phrase = "High Dive"
+print(character_name + " Goes to " + phrase)
+print(phrase.lower())
+print(phrase.isupper())
+print(phrase.islower())
+print(phrase.upper().isupper())
+
+#find position in string/grab character in string
+print(phrase[6])
+
+#find where word is indexed
+print(phrase.index("Dive"))
+
+#replace word in string
+print(phrase.replace("Dive", "Donkey"))
+
+#working with numbers
+print(2 + 2 == (2 * 1 + 2 * 1))
+
+#mod how many times it goes in
+print(10 % 3)
+print(3 % 10)
+
+#variables
+my_num_2 = -26
+my_num = 26
+print(my_num)
+
+#absolute number *just the number
+print(abs(my_num_2))
+
+#numbers to the power of eg. 4*4 for this example
+print(pow(2,4))
+
+
+
